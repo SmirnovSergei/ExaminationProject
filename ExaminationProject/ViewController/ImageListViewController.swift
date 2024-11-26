d7af1a29fcb57da41e26ba0dee89a96af23fba17
Smirnov Sergey <fost@live.ru>
2024-11-27 00:25:26 +0300

    Создан вью контроллер для 33 урока

---
+++ ExaminationProject/ViewController/ImageListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageListViewController.swift
+//  ExaminationProject
+//
+//  Created by Сергей Смирнов on 27.11.2024.
+//
+
+import UIKit
+
+class ImageListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
